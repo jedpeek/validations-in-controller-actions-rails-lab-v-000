aa15072295dc954cdf22db86c2bde257a19f5274
Jed Peek <jedpeek@yahoo.com>
2018-08-17 18:23:10 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,7 @@ class PostsController < ApplicationController
   end
 
   def update
-  @post = Post.update(post_params)
+  @post = Post.find_by_id(post_params)
     if @post.valid?
       @post.save
       redirect_to post_path(@post)
